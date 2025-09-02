041a8bfa9f52b5da18f0c064cb95d3ac204efc50
Richard Rhee <rdrhee@gmail.com>
2025-09-02 16:10:11 -0500

    Remove old arch dotfiles and scripts

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar left   -c ~/.config/polybar/config.ini &
-polybar center -c ~/.config/polybar/config.ini &
-polybar right  -c ~/.config/polybar/config.ini &
-
--- Scripts/display.sh
+++
@@ -1,1 +0,0 @@
-xrandr --output HDMI-1 --mode 1920x1200 --left-of eDP-1
